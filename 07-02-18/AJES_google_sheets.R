899a33964ab41448763cd5d6dbf71c503e524899
aes123 <edmondson.staita@gmail.com>
2018-02-07 14:34:24 +0000

    col_names = FALSE

--- 07-02-18/AJES_google_sheets.R
+++ 07-02-18/AJES_google_sheets.R
@@ -8,7 +8,7 @@ y5 <- gs_title("Upstream SMP (PAM-y5)")
 #Problem solve:
 gs_read(ss = y5, ws = 4, range = "B52:B73")
 as.list(gs_read(ss = y5, ws = 4, range = "B52:B73"))
-skids<- as.list(gs_read(ss = y5, ws = 4, range = "B52:B73"))[[1]] #misses first one in list
+skids<- as.list(gs_read(ss = y5, ws = 4, range = "B52:B73"), col_names = FALSE)[[1]] #misses first one in list
 
 
 library(elmr)
@@ -17,7 +17,7 @@ df <- lapply(c(1:length(skids)), function(x) summary(results[[x]]))
 names(df) <- lapply(c(1:length(skids)), function(x) catmaid_get_neuronnames(skids[[x]]))
 index <- which(names(df)=="neuron 3336471 JJ- FW-B6")
 
-#Make a function that updates google sheet to add info about already connected neurons
+#Function that updates google sheet to add info about already connected neurons
 neuron_names <- as.vector(catmaid_get_neuronnames(skids))
 #gs_edit_cells(y5, ws = 4, input = neuron_names, anchor = "M53", byrow = FALSE)
 
