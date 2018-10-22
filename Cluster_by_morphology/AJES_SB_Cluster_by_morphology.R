b88bd4f90726a595d2e06c31be8a3a7c41061dc8
AmeliaES <aje49@cam.ac.uk>
2018-10-22 15:37:46 +0100

    Tidy code.

--- Cluster_by_morphology/AJES_SB_Cluster_by_morphology.R
+++ Cluster_by_morphology/AJES_SB_Cluster_by_morphology.R
@@ -49,10 +49,6 @@ dataset_annotation <- "NAMK_DANs_all"
 all_annotations <- catmaid_get_annotations_for_skeletons(catmaid_skids("annotation:NAMK_DANs_all"))
 colour_by_annotation <- sort(unique(all_annotations$annotation[grep("NAMK_DANs_lin",all_annotations$annotation)]))
 
-#dataset_annotation <- "FML - downstream of DA2"
-#colour_by_annotation <- "FML - downstream of first DA2 - initial sample"
-#other_annotation_label <- "second sample"
-
 #This takes a while to run, but once the NBLAST is done it will save it as a variable.
 col.dendro <- cluster_function(dataset_annotation = dataset_annotation, colour_by_annotation = colour_by_annotation)
 
@@ -70,6 +66,3 @@ legend("topright",
        pch = 20,
        cex = 0.75,
        ncol = 3)
-#pch = c(20,20,4,4,4), bty = "n",  pt.cex = 1.5, cex = 0.8 , 
-#text.col = "black", horiz = FALSE, inset = c(0.1, 0.1))
-
