6f227f15a89a09ed879e50ef63c2fce1aab22e9d
AmeliaES <aje49@cam.ac.uk>
2018-11-06 12:51:46 +0000

    Added more comments.

--- AJES_cumulative_count.R
+++ AJES_cumulative_count.R
@@ -1,24 +1,27 @@
 library(googlesheets)
 #read list of neuron names sampled from google sheets
 y5 <- gs_title("Upstream SMP (PAM-y5)")
-data <- gs_read(ss = y5, ws = 2, range = "K3:K100")
-neuron_name <- data$`Final Neuron ID`
+data <- gs_read(ss = y5, ws = 3, range = "K3:K100")
+neuron_name <- data$`Final Neuron Name`
 
-#sampled y5-PAM upstream neurons
-neuron_name <- unlist(lapply(c(2:7), function(x) as.data.frame(gs_read(ss = y5, ws = x, range = "L3:L500"))[,1]))
+#neurons upstream of sampled y5-PAM upstream neurons
+neuron_name <- unlist(lapply(c(2:7), function(x) as.data.frame(gs_read(ss = y5, ws = x, range = "K3:K500"))[,1]))
 
 ###### Give function list of neuron names or skids and it will plot if it is a new neuron, an exisiting neuron, or a gap in the list (ie. a fragment) ####
 
 sampling <- function(neuron_name){
-logical_new_or_exisiting <- unlist(lapply(1:length(neuron_name), function(x) length(which(neuron_name[c(1:x)] %in% neuron_name[x])) >= 2 )) #FALSE means it's a new neuron, TRUE means it's an existing neuron
-logical_new_or_exisiting[which(neuron_name %in% NA)] <- NA #Indices of neuron_name at which a neuron has not been found, ie. it is a fragment
-#If FALSE new_neurons = new_neurons + 1, if TRUE new_neurons = new_neurons, if NA fragments = fragments + 1
-
-sum_up <- data.frame('Name' = neuron_name, 'Cumulative new neurons' = rep(0,length(neuron_name)))
+#return a logical vector FALSE means it's a new neuron, TRUE means it's an existing neuron
+logical_new_or_exisiting <- sapply(1:length(neuron_name), function(x) sum(neuron_name[c(1:x)] %in% neuron_name[x]) >= 2 )
+#Indices of neuron_name at which a neuron has not been found and is empty in original list of neuronnames read in from googlesheet, ie. it is still a fragment
+logical_new_or_exisiting[which(is.na(neuron_name))] <- NA 
+#Make an empty data frame to make a cumulative count of whether it's a new or existing neuron, or a fragment
+sum_up <- data.frame('Name' = neuron_name, 'Cumulative new neurons' = 0)
+#If logical_new_or_exisiting is FALSE: new_neurs = new_neurs + 1,
+#if TRUE: new_neurs = new_neurs, 
+#if NA then insert a 0 as it is neither an existing neuron nor a new neuron (likely a fragment)
 new_neurs <- 0
-
-for (index in 1:length(logical_new_or_exisiting)) {
-    if (FALSE %in% logical_new_or_exisiting[index]) {
+for (i in 1:length(logical_new_or_exisiting)) {
+    if (FALSE %in% logical_new_or_exisiting[i]) {
       new_neurs = new_neurs + 1
       } else if(TRUE %in% logical_new_or_exisiting[index]) {
       new_neurs = new_neurs
