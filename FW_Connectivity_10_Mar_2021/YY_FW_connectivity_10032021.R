219378a41d59d892e52df37d95c00eed0a481ad8
YijieYin <37304606+YijieYin@users.noreply.github.com>
2021-03-12 11:57:22 +0000

    Add files via upload

--- FW_Connectivity_10_Mar_2021/YY_FW_connectivity_10032021.R
+++ FW_Connectivity_10_Mar_2021/YY_FW_connectivity_10032021.R
@@ -1,11 +1,10 @@
 # This script intends to: 
 # Find downstream partners of 720575940603560294 (LHa1 hemilineage)
-# Get the neuron’s top five partners based on the number of synapses
+# Get the neuron's top five partners based on the number of synapses
 # Plot the neuron and each partner (include the synapses in the plot) for all five neurons, and have the synapses colour coded by partner
 # Advance task
 # Construct a neuroglancer scene with the neuron of interest and its top five partners (note: the top five partners should be the same color as it's relevant synapse colour assigned in point 3)
 # NBLAST connectivity matrix of the LHa1 hemilineage [thresholds 1. include adequate/complete neurons 2. include connections with more than 5 synapses per neuron]
-
 library(natverse)
 library(fafbseg)
 library(googlesheets4)
@@ -14,49 +13,79 @@ library(hemibrainr)
 library(ComplexHeatmap)
 library(tidyverse)
 
-id <- '720575940603560294'
+# Plot top 5 downstream partners ####
+id <- flywire_latestid('720575940603560294')
 
+# get top 5 downstream ids 
 n.downstream <- flywire_partner_summary(id, method = 'spine')
-n <- read_cloudvolume_meshes(id)
-plot3d(n, col = 'red', WithNodes = F)
-# This doesn't work. 
-flywire_ntplot3d(id, plot = 'points', size = 5)
-
 top5.ids <- n.downstream$post_id[1:5]
-top5 <- read_cloudvolume_meshes(top5.ids)
-plot3d(top5, add = TRUE)
 
-# Advance: 
+# get meshes for plotting 
+n <- read_cloudvolume_meshes(id)
+top5 <- read_cloudvolume_meshes(top5.ids)
+neuron_colour <- data.frame(
+  ids = top5.ids, 
+  colours = c('red','green','blue','yellow','pink')
+)
+
+# get synapses 
+syn <- flywire_ntpred(id)
+# syn$post_id is originally double, making it hard to be compared with the ones we have 
+syn$post_id <- as.character(syn$post_id)
+syn1 <- syn[syn$post_id %in% top5.ids,]
+
+# add colours to the df, matching the ids 
+syn1 <- merge(syn1, neuron_colour, by.x = 'post_id', by.y = 'ids', all.x = T)
+flywire_ntplot3d(syn1, col = syn1$colours)
+
+# Plot! 
+plot3d(n, add = T, col = 'brown', alpha = 0.5)
+plot3d(top5, add = TRUE, col = neuron_colour$colours, alpha = 0.2)
+points3d(syn1$pre_x, syn1$pre_y, syn1$pre_z, col = syn1$colours)
+
+
+# Flywire URL#### 
 # get sample FlyWire URL
 fw_url=with_segmentation('flywire', getOption('fafbseg.sampleurl'))
 fw_sc=ngl_decode_scene(fw_url)
-fw_sc$layers[[2]]$segments=union(fw_sc$layers[[2]]$segments,c(id, top5.ids)) 
-url <- ngl_encode_url(fw_sc)
-
+url <- as.character(fw_sc + c(id,top5.ids) - 0)
 url.colors <- as.character(ngl_add_colours(url, data.frame(
   ids <- c(id, top5.ids), 
-  col <- c('red','blue', 'green', 'yellow', 'orange', 'pink')
+  col <- c('brown',neuron_colour$colours)
 )))
 # This line opens the url: 
-# browseURL(url.colors)
+browseURL(url.colors)
 
+
+# Adjancency matrix and NBLAST ####
 LHa1.he <- read_sheet('1QyuHFdqz705OSxXNynD9moIsLvZGjjBjylx5sGZP2Yg', sheet = 'ItoLee_Hemilineage_LHa1_right')
+# You need to make sure they are unique, otherwise flywire_partner_summary() finds it twice 
 LHa1.ac.ids <- unique(flywire_xyz2id(LHa1.he$flywire.xyz[LHa1.he$status %in% c('adequate', 'complete', 'a', 'c')], rawcoords = T)) 
 
+# get connectivity between these neurons 
 LHa1.in <- flywire_partner_summary(LHa1.ac.ids, partners = 'inputs', method = 'spine')
 LHa1.in5.lha1 <- LHa1.in[LHa1.in$weight > 5 & LHa1.in$pre_id %in% LHa1.ac.ids,]
 
+# This if for ggplot, in the format of: 
+# pre post weight
+# 1   2     0
+# 1   3     1
+# 1   4     0
+# 2   1     0
+# 2   2     0
+# ...
 conn <- expand.grid(pre_ids = LHa1.ac.ids, post_ids = LHa1.ac.ids)
 conn <- merge(conn, LHa1.in5.lha1, 
               by.x = c('pre_ids', 'post_ids'), by.y = c('pre_id', 'query'), 
               all.x = T)
 conn$weight[is.na(conn$weight)] <- 0
 
-# morphological clustering 
+# morphological clustering
 # skeletonise yourself - can you get skeletons from google drive? 
-fw.n <- flywire_neurons()
 read_cloudvolume_meshes(LHa1.ac.ids, savedir = '/Users/yijieyin/Downloads/LHa1')
 ff = file.path('/Users/yijieyin/Downloads/LHa1', paste0(LHa1.ac.ids,'.obj'))
+# This downloads the mesh again anyway, despite the downloading above 
+# If it gives you failures with some ids, just try again 
 LHa1.sk <- skeletor(ff)
 LHa1.dps <- dotprops(LHa1.sk/1000, k = 5, resample = 1)
 LHa1.scores <- nblast_allbyall(LHa1.dps)
@@ -72,7 +101,6 @@ hm <- ggplot(data = conn, mapping = aes(x = pre_ids,
   scale_fill_gradient(low = 'white', high = 'red') + 
   theme(axis.text.x = element_blank(), 
         axis.text.y = element_blank())
-
 hm
 
 # This gives the clustering you want 
@@ -85,6 +113,4 @@ Heatmap(data.matrix(mtx1),
         show_column_names = FALSE, 
         cluster_rows = LHa1.hc, 
         cluster_columns = F)
-
-
 
